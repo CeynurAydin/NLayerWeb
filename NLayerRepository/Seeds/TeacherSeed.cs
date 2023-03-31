433df890ca6a678bf1b749df6990bcfe0fe6da35
ceynur aydın <ceynur.aydin@gmail.com>
2023-04-01 00:15:03 +0300

    seed data eklendi

---
+++ NLayerRepository/Seeds/TeacherSeed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayerCore.Model;
+
+
+namespace NLayerRepository.Seeds
+{
+    internal class TeacherSeed : IEntityTypeConfiguration<Teacher>
+    {
+        public void Configure(EntityTypeBuilder<Teacher> builder)
+        {
+            builder.HasData(
+               new Teacher { Id = 1, Name = "Ceynur", SurName = "Aydın", GraduateDepartment = "EEM", ExamScore = 74.2, OsymRank = 203, PreferredSection = "Electronic", PreferredCity = "Istanbul" });
+              
+        }
+    }
+}
